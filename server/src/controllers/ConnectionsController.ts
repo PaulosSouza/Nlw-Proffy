9ddcfe0d4861f58d8bde60a4514f1f81760f5732
Paulo Henrique <phferreirasouza106@gmail.com>
2020-08-08 00:28:01 -0300

    Server done

---
+++ server/src/controllers/ConnectionsController.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from 'express';
+
+import db from '../database/connection';
+
+class ConnectionsController {
+  async index(request: Request, response: Response): Promise<Response> {
+    const totalConnections = await db('connections').count('* as total');
+
+    const { total } = totalConnections[0];
+
+    return response.json({ total });
+  }
+
+  async create(request: Request, response: Response): Promise<Response> {
+    const { user_id } = request.body;
+
+    const trx = await db.transaction();
+
+    await trx('connections').insert({
+      user_id,
+    });
+
+    trx.commit();
+
+    return response.status(201).send();
+  }
+}
+
+export default ConnectionsController;
